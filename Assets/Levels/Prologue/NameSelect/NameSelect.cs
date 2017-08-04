f119e25d94a8b8b41dfc1bec08f5725f1afd9808
Kevin Lai <kevinlai.us@gmail.com>
2017-08-03 20:20:07 -0400

    Selection cursor works independently of NameSelect

--- Assets/Levels/Prologue/NameSelect/NameSelect.cs
+++ Assets/Levels/Prologue/NameSelect/NameSelect.cs
@@ -7,7 +7,7 @@ namespace Game.Levels
     [RequireComponent(typeof(SelectionCursor))]
     public class NameSelect : MonoBehaviour
     {
-        [SerializeField] SelectionCursor selectionArrow;
+        [SerializeField] SelectionCursor selectionCursor;
 
         public delegate void BroadcastNameSelected();
         public event BroadcastNameSelected NotifyNameSelected;
@@ -16,27 +16,25 @@ namespace Game.Levels
         const string DEFAULT_NAME = "Mason";
         const int MAX_NAME_LENGTH = 13;
 
-        Text selectedLetter;
         [SerializeField] Text letterPrefab;
         [SerializeField] GameObject letterGrid;
 
         const int LETTER_GRID_X = 7;
         const int LETTER_GRID_Y = 8;
-        Text[,] textGrid = new Text[LETTER_GRID_X, LETTER_GRID_Y];
 
         void Start()
         {
+            selectionCursor.MenuGrid = new Text[LETTER_GRID_X, LETTER_GRID_Y];
             PopulateLetterGrid();
-            selectedLetter = textGrid[0, 0];
+            selectionCursor.SelectedMenuItem = selectionCursor.MenuGrid[0, 0];
         }
 
         void Update()
         {
-            ProcessArrowInput();
             ProcessCommandInput();
         }
 
-        private void ProcessCommandInput()
+        void ProcessCommandInput()
         {
             if (Input.GetKeyDown(KeyCode.V))
             {
@@ -59,69 +57,53 @@ namespace Game.Levels
             }
         }
 
-        private void ProcessArrowInput()
+        void SelectDefaultName()
         {
-            // Y values are reversed because the grid opens downwards
-            if (selectionArrow.TimePassedSinceKey > SelectionCursor.KEY_DELAY)
+            selectionCursor.PlayAudio(SelectionCursor.CursorSounds.Default);
+            userNameSelection.text = DEFAULT_NAME;
+        }
+
+        void SelectCharacter()
+        {
+            if (userNameSelection.text.Length < MAX_NAME_LENGTH)
             {
-                if (Input.GetKey(KeyCode.W))
-                {
-                    if (selectionArrow.SelectIndex.y > 0)
-                    {
-                        selectionArrow.SelectIndex.y -= 1;
-                    }
-                    else
-                    {
-                        selectionArrow.SelectIndex.y = LETTER_GRID_Y - 1;
-                    }
+                selectionCursor.PlayAudio(SelectionCursor.CursorSounds.Select);
 
-                    MoveArrow();
-                }
-
-                else if (Input.GetKey(KeyCode.S))
-                {
-                    if (selectionArrow.SelectIndex.y < LETTER_GRID_Y - 1)
-                    {
-                        selectionArrow.SelectIndex.y += 1;
-                    }
-                    else
-                    {
-                        selectionArrow.SelectIndex.y = 0;
-                    }
-
-                    MoveArrow();
-                }
-
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    if (selectionArrow.SelectIndex.x < LETTER_GRID_X - 1)
-                    {
-                        selectionArrow.SelectIndex.x += 1;
-                    }
-                    else
-                    {
-                        selectionArrow.SelectIndex.x = 0;
-                        if (selectionArrow.SelectIndex.y < LETTER_GRID_Y - 1) selectionArrow.SelectIndex.y += 1;
-                    }
-
-                    MoveArrow();
-                }
-
-                else if (Input.GetKey(KeyCode.A))
-                {
-                    if (selectionArrow.SelectIndex.x > 0)
-                    {
-                        selectionArrow.SelectIndex.x -= 1;
-                    }
-                    else
-                    {
-                        selectionArrow.SelectIndex.x = LETTER_GRID_X - 1;
-                        if (selectionArrow.SelectIndex.y > 0) selectionArrow.SelectIndex.y -= 1;
-                    }
-
-                    MoveArrow();
-                }
+                userNameSelection.text += ((Text)selectionCursor.SelectedMenuItem).text;
+                selectionCursor.GetAnimator().SetTrigger(SelectionCursor.SELECT_TRIGGER);
             }
+            else
+            {
+                selectionCursor.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
+            }
+        }
+
+        void BackSpace()
+        {
+            selectionCursor.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
+
+            if (userNameSelection.text != "")
+            {
+                userNameSelection.text = userNameSelection.text.Substring(0, userNameSelection.text.Length - 1);
+            }
+        }
+
+        void ConfirmNameSelection()
+        {
+            string selectedName = userNameSelection.text.Trim();
+            if (selectedName == "")
+            {
+                selectionCursor.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
+                userNameSelection.text = "";
+                return;
+            }
+
+            Game.Actors.PlayerData.PlayerName = selectedName;
+            selectionCursor.PlayAudio(SelectionCursor.CursorSounds.Confirm);
+
+            NotifyNameSelected();
+            gameObject.transform.SetParent(Camera.main.transform);
+            Destroy(this);
         }
 
         void PopulateLetterGrid()
@@ -169,69 +151,9 @@ namespace Game.Levels
         Text AddCharToGrid(int x, int y, int charIndex)
         {
             char charToAdd = (char)charIndex;
-            textGrid[x, y] = Instantiate(letterPrefab, letterGrid.transform);
-            textGrid[x, y].text += charToAdd;
-            return textGrid[x, y];
-        }
-
-        private void SelectDefaultName()
-        {
-            selectionArrow.PlayAudio(SelectionCursor.CursorSounds.Default);
-            userNameSelection.text = DEFAULT_NAME;
-        }
-
-        void SelectCharacter()
-        {
-            if (userNameSelection.text.Length < MAX_NAME_LENGTH)
-            {
-                selectionArrow.PlayAudio(SelectionCursor.CursorSounds.Select);
-
-                userNameSelection.text += selectedLetter.text;
-                selectionArrow.GetAnimator().SetTrigger(SelectionCursor.SELECT_TRIGGER);
-            }
-            else
-            {
-                selectionArrow.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
-            }
-        }
-
-        void BackSpace()
-        {
-            selectionArrow.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
-
-            if (userNameSelection.text != "")
-            {
-                userNameSelection.text = userNameSelection.text.Substring(0, userNameSelection.text.Length - 1);
-            }
-        }
-
-        void MoveArrow()
-        {
-            selectionArrow.TimePassedSinceKey = 0;
-            selectionArrow.PlayAudio(SelectionCursor.CursorSounds.Move);
-
-            Text highlightedLetter = textGrid[(int)selectionArrow.SelectIndex.x, 
-                (int)selectionArrow.SelectIndex.y];
-            selectionArrow.transform.position = highlightedLetter.transform.position + selectionArrow.GetOffset();
-            selectedLetter = highlightedLetter;
-        }
-
-        void ConfirmNameSelection()
-        {
-            string selectedName = userNameSelection.text.Trim();
-            if (selectedName == "")
-            {
-                selectionArrow.PlayAudio(SelectionCursor.CursorSounds.CannotSelect);
-                userNameSelection.text = "";
-                return;
-            }
-
-            Game.Actors.PlayerData.PlayerName = selectedName;
-            selectionArrow.PlayAudio(SelectionCursor.CursorSounds.Confirm);
-
-            NotifyNameSelected();
-            gameObject.transform.SetParent(Camera.main.transform);
-            Destroy(this);
+            selectionCursor.MenuGrid[x, y] = Instantiate(letterPrefab, letterGrid.transform);
+            ((Text)selectionCursor.MenuGrid[x, y]).text += charToAdd;
+            return ((Text)selectionCursor.MenuGrid[x, y]);
         }
 
     }
