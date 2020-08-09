52abf810c25c761bd0d074cb5176eab2231ff0a2
klai1994 <kevinlai.us@gmail.com>
2020-08-08 21:08:12 -0400

    Created script for a pause menu, outlining what is needed for an "options" screen.

---
+++ Assets/UI/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    /*
+    Pause menu should include:
+    Save/Load
+    Controls
+    Volume
+    Return
+    Exit Game
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
